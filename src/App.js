61d169f01e71eadb591814c734de2195f759652d
SP Mohanty <sharada.mohanty@epfl.ch>
2017-11-13 21:05:47 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import STNavBar from './STNavBar.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <STNavBar/>
+    );
+  }
+}
+
+export default App;
